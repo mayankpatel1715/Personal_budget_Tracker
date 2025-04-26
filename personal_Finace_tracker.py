610e794940a8a318a1cda87b2e8f8f01961adaf6
Mayank2002Patel <165916460+Mayank2002Patel@users.noreply.github.com>
2025-04-26 16:35:23 +0530

    1.1

---
+++ personal_Finace_tracker.py
@@ -0,0 +1,24 @@
+import math
+
+# Income 
+def income():
+    '''
+    Here You can add your income
+    '''
+    income = int(input("Enter your Income: ₹"))
+    return income
+
+print(income())
+
+# Split
+def split():
+    '''
+    Here you can split your money on the basis of:
+    Needs --> 40%
+    Wants --> 30%
+    Investments --> 30%
+    '''
+    
+
+
+# Transcactions
