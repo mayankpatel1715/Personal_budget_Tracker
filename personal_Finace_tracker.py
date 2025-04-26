da59602341706a91d868f36806fbef143cdedce4
Mayank2002Patel <165916460+Mayank2002Patel@users.noreply.github.com>
2025-04-26 17:22:46 +0530

    1.3

--- personal_Finace_tracker.py
+++ personal_Finace_tracker.py
@@ -5,6 +5,34 @@ import math
 income = int(input("Enter your monthly income: ₹"))
 
 # Split
+def split():
+    '''
+    Here you can mange your money on the basis of needs, wants and investments
+    '''
+    needs = int(input("Enter your needs percentage:"))
+    wants = int(input("Enter your wants percentage:"))
+    investments = int(input("Enter your invesments percentage:"))
+    
+    if needs + wants + investments != 100:
+        print("Enter Valid Number")
+        return
+    
+    
+
+    needs = int(needs*income/100)
+    wants = int(wants*income/100)
+    investments = int(investments*income/100)
+
+    print(f"Your needs are :₹{needs}")
+    print(f"Your wants are :₹{wants}")
+    print(f"Your invesments are :₹{investments}")
+
+    return 
+
+
+
+split()
+
 
 
 # Transcactions
